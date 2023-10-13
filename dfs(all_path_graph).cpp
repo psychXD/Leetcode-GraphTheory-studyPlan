4eec037689c1fe45421280ff1d112c333b14b0b8
Harsh Vardhan Singh <harshvs469@gmail.com>
2023-10-13 23:20:31 +0530

    Add files via upload

---
+++ dfs(all_path_graph).cpp
@@ -0,0 +1,21 @@
+//https://leetcode.com/problems/all-paths-from-source-to-target/?envType=study-plan-v2&envId=graph-theory
+
+void dfs(vector<vector<int>>& graph,  vector<vector<int>>& ans, vector<int>& visited, int node){
+        visited.push_back(node);
+        if(node == graph.size()-1){
+            ans.push_back(visited);
+        }
+        else{
+            for(int i=0; i<graph[node].size(); i++){
+                dfs(graph, ans, visited, graph[node][i]);
+            }
+        }
+        visited.pop_back();
+    }
+    vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
+        vector<int> visited;
+        vector<vector<int>> ans; 
+        dfs(graph,ans,visited,0);
+       
+        return ans;
+    }
